541fcaaffe64cd512f9cac9c51f3d7f9bcefd4aa
AlexTicovschi <alexmticovschi@gmail.com>
2018-08-17 10:06:09 +0100

    react app files

---
+++ src/App.js
@@ -0,0 +1,148 @@
+import React, { Fragment, Component } from 'react';
+import { Container, Button, Form, FormGroup, Input } from 'reactstrap';
+
+import axios from 'axios';
+import Recipes from './components/Recipes';
+import { ClipLoader } from 'react-spinners';
+
+import './App.css';
+
+
+class App extends Component {
+  state = {
+    recipes: [],
+    keywords: '',
+    loading: false
+}
+
+componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    //   when user leaves/refreshes the page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    //window.onload = this.getRecipe();
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+    
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+
+getRecipe = (event) => {
+    event.preventDefault();
+    this.setState({recipes: []});
+    
+    //const proxy = 'https://cors-anywhere.herokuapp.com/';
+    // const proxy = 'https://cryptic-headland-94862.herokuapp.com/';
+    const API_KEY = '4921fea5b819539cd1fd95afc554ea9e';
+    //const API_KEY2 = '462b1cc8d4f2730081462fbc65136320';
+    
+    this.setState({loading: true});
+    
+    axios.get(`http://food2fork.com/api/search?key=${API_KEY}&q=${this.state.keywords}`)
+        .then(response => {
+            const fetchedRecipes = [];
+            console.log(response.data.recipes);
+            response.data.recipes.map(recipe => fetchedRecipes.push(recipe));
+            this.setState({recipes: fetchedRecipes});
+
+            this.setState({loading: false});
+            
+            console.log('[STATE RECIPES]:',this.state.recipes);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    
+}
+
+clearInput = (e) => { 
+    e.target.value= '';
+    this.setState({recipes: []});
+}
+
+onInputChange = (event) => {
+    this.setState({ keywords: event.target.value });
+    
+    console.log('[State keywords]', this.state.keywords);
+}
+
+render() {
+    return (
+        <Fragment>
+            <Container>
+                {/* <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
+                    <header className="App-header">
+                        {/* <img src="assets/img/background2.jpg" width="100%" alt=""/> */}
+                        <div className="logo">Recipe Finder</div>
+                         <div className="box">
+                                
+                            <Form inline className="search" ref={(el) => this.myFormRef = el}>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Input type="text" placeholder="Search recipe..." onChange={this.onInputChange}  />
+                                </FormGroup>{' '}
+                                <Button 
+                                    className="btn" 
+                                    style={{backgroundColor:"#21a00b", color:"#fff", outline:"none"}} 
+                                    onClick={this.getRecipe} type="submit">Search Recipe</Button>   
+                            </Form>                                   
+                        </div>
+                    </header>
+
+                    <div className='sweet-loading'>
+                        <ClipLoader 
+                            size={100}
+                            color={'#21a00b'} 
+                            loading={this.state.loading} />
+                    </div>
+                    
+                    <Recipes 
+                        keywords={this.state.keywords} 
+                        recipes={this.state.recipes} /> 
+                
+            </Container>
+        </Fragment>
+    )
+}
+
+}
+
+export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap';
+import { Button } from 'reactstrap';
+
+import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
+import { Animated } from "react-animated-css";
+
+
+class MyModal extends Component {
+    constructor(props, context) {
+      super(props, context);
+  
+      this.handleShow = this.handleShow.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+  
+      this.state = {
+        show: false,
+        id: '',
+        recipes: [],
+        ingredients: [],
+        loading: false
+      };
+    }
+
+    getRecipe = (id) => {
+      //const proxy = 'https://cryptic-headland-94862.herokuapp.com/';
+      //const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+      const API_KEY = '4921fea5b819539cd1fd95afc554ea9e';
+      //const API_KEY2 = '462b1cc8d4f2730081462fbc65136320';
+
+      this.setState({loading: true});
+
+      axios.get(`http://food2fork.com/api/get?key=${API_KEY}&rId=${id}`)
+        .then(response => {
+          
+
+          const fetchedIngredients = [];
+          //console.log('[Ingredients]',response.data.recipe.ingredients);
+          response.data.recipe.ingredients.map(ing => fetchedIngredients.push(ing));
+          this.setState({ ingredients: fetchedIngredients });
+          //console.log('[STATE INGREDIENTS]',this.state.ingredients)
+          //console.log('[STATE RECIPES]:',this.state.recipes);
+          this.setState({loading: false});
+      })
+      .catch(error => {
+          console.log(error);
+      });
+    }
+
+    handleClose() {
+      this.setState({ show: false, ingredients: [] }); 
+    }
+  
+    handleShow() {
+      this.setState({ show: true, id: this.props.id });
+      //console.log(this.state.id)
+      this.getRecipe(this.props.id);
+    }
+  
+    render() {
+      //console.log('[INSIDE Modal.js]',this.props);
+      //console.log('[PROPS RECIPIES]',this.state.recipies);
+      
+      //console.log('[INSIDE Modal.js STATE]',this.state.id);
+      // console.log('[INSIDE Modal.js keywords]',this.props.keywords);
+  
+      return (
+        <div>
+          {/* <Button style={{backgroundColor:"#21a00b", color:"#fff", outline: "none", marginTop:"100px", marginBottom:"0px"}} onClick={this.handleShow} block> */}
+          <Button className="btn" style={{backgroundColor:"#21a00b", color:"#fff", outline: "none", marginTop:"22px", marginBottom:"10px"}} onClick={this.handleShow} block>
+            View Recipe
+          </Button>
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton style={{backgroundColor: "#9de88f"}}>
+              {/* <Modal.Title></Modal.Title> */}
+              <h3 className="text-center"><img className="food_logo" src="assets/img/food-logo.png" height="60px" alt=""/>{this.props.title}</h3>
+            </Modal.Header>
+            
+            <div className="text-center">
+              <img style={{
+                    marginTop: '10px'
+                  }} src={this.props.image_url}  height='400' width='94%' alt="" />
+            </div>
+            
+            <Modal.Body>
+            
+              <h4 className="text-center">
+                <img className="publisher_logo" src="assets/img/pub.jpg" height="60px" alt=""/>
+                  Publisher: <a href={this.props.publisher_url} target="_blank">{this.props.publisher_name}</a>
+              </h4>
+                
+              <a className="source_btn btn" style={{color:"#fff"}} href={this.props.source_url} target="_blank">
+                  View Recipe Source 
+              </a>
+              
+              <hr />
+              <p className="text-center" style={{fontSize: "21px", backgroundColor: "#21a00b", color: "#fff", padding:"5px 0px"}}> *** Ingredients *** </p>
+              <div className='spinner'>
+                <ClipLoader
+                  size={70}
+                  color={'#21a00b'} 
+                  loading={this.state.loading} 
+                />
+              </div>
+              <ul>
+                  {this.state.ingredients.map((ing, i) => (
+                    <Animated animationIn="bounceInUp" animationOut="fadeOut" isVisible={true}>
+                    <li className="leaf" key={i} style={{fontSize: "17px"}}>{ing}</li>
+                    </Animated>
+                  ))}
+              </ul>
+              
+            </Modal.Body>
+            <Modal.Footer>
+              <Button style={{backgroundColor:"#21a00b", color:"#fff", outline:"none"}}  onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      );
+    }
+  }
+ 
+export default MyModal;
