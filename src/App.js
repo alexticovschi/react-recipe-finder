92651bb7e89533437e76be60a85e922497f78be9
AlexTicovschi <alexmticovschi@gmail.com>
2018-08-21 12:15:44 +0100

    starring favorite recipes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Container, Button, Form, FormGroup, Input } from 'reactstrap';
 
 import axios from 'axios';
 import RecipeList from './components/RecipeList';
-import FavoriteRecipes from './components/FavoriteRecipes'
+// import FavoriteRecipes from './components/FavoriteRecipes'
 import { ClipLoader } from 'react-spinners';
 
 import './App.css';
@@ -15,9 +15,9 @@ class App extends Component {
   state = {
     keywords: '',
     loading: false
-}
+  }
 
-componentDidMount() {
+  componentDidMount() {
     this.hydrateStateWithLocalStorage();
 
     // add event listener to save state to localStorage
@@ -67,126 +67,94 @@ componentDidMount() {
     }
   }
 
-//   addToFavorites = (recipe) => {
-
-//     this.setState({
-//       favorites: [...this.state.favorites, recipe]
-//     });
-
-//     console.log('[FAVORITE RECIPES]:', this.state.favorites);
-
-//   }
-
   getRecipe = (event) => {
-      event.preventDefault();
-      this.setState({recipes: []});
+    event.preventDefault();
+    this.setState({recipes: []});
 
-      //const proxy = 'https://cors-anywhere.herokuapp.com/';
-      // const proxy = 'https://cryptic-headland-94862.herokuapp.com/';
-      const API_KEY = '4921fea5b819539cd1fd95afc554ea9e';
-      //const API_KEY2 = '462b1cc8d4f2730081462fbc65136320';
+    //const proxy = 'https://cors-anywhere.herokuapp.com/';
+    // const proxy = 'https://cryptic-headland-94862.herokuapp.com/';
+    const API_KEY = '4921fea5b819539cd1fd95afc554ea9e';
+    //const API_KEY2 = '462b1cc8d4f2730081462fbc65136320';
       
-      this.setState({loading: true});
-      
-    //   axios.get(`http://food2fork.com/api/search?key=${API_KEY}&q=${this.state.keywords}`)
-    //       .then(response => {
-    //           const fetchedRecipes = [];
-    //           console.log(response.data.recipes);
-    //           response.data.recipes.map(recipe => fetchedRecipes.push(recipe));
-    //           this.setState({recipes: fetchedRecipes});
+    this.setState({loading: true});
+      axios.get(`http://food2fork.com/api/search?key=${API_KEY}&q=${this.state.keywords}`)
+        .then(response => {
+          const fetchedRecipes = [];
+          //console.log(response.data.recipes);
+          response.data.recipes.map(recipe => fetchedRecipes.push(recipe));
+          this.props.setRecipes(response.data.recipes);
 
-    //           this.setState({loading: false});
-              
-    //           console.log('[STATE RECIPES]:',this.state.recipes);
-    //   })
-    //   .catch(error => {
-    //       console.log(error);
-    //   });
-        axios.get(`http://food2fork.com/api/search?key=${API_KEY}&q=${this.state.keywords}`)
-            .then(response => {
-                const fetchedRecipes = [];
-                //console.log(response.data.recipes);
-                response.data.recipes.map(recipe => fetchedRecipes.push(recipe));
-                this.props.setRecipes(response.data.recipes);
-
-                this.setState({loading: false});
+          this.setState({loading: false});
                 
-                //console.log('[STATE RECIPES]:',this.state.recipes);
+          //console.log('[STATE RECIPES]:',this.state.recipes);
         })
         .catch(error => {
             console.log(error);
-        })
-      
+        })  
   }
 
   clearInput = (e) => { 
-      e.target.value= '';
-      this.setState({recipes: []});
+    e.target.value= '';
+    this.setState({recipes: []});
   }
 
   onInputChange = (event) => {
-      this.setState({ keywords: event.target.value });
+    this.setState({ keywords: event.target.value });
       
-      //console.log('[State keywords]', this.state.keywords);
+    //console.log('[State keywords]', this.state.keywords);
   }
 
   render() {
-    
-      return (
-          <Fragment>
-              <Container>
-                  {/* <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
-                      <header className="App-header">
-                          {/* <img src="assets/img/background2.jpg" width="100%" alt=""/> */}
-                          <div className="logo">Recipe Finder</div>
-                          <div className="box">
+    return (
+      <Fragment>
+        <Container>
+          {/* <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
+          <header className="App-header">
+            {/* <img src="assets/img/background2.jpg" width="100%" alt=""/> */}
+            <div className="logo">Recipe Finder</div>
+            <div className="box">
                                   
-                              <Form inline className="search" ref={(el) => this.myFormRef = el}>
-                                  <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                                      <Input type="text" placeholder="Search recipe..." onChange={this.onInputChange}  />
-                                  </FormGroup>{' '}
-                                  <Button 
-                                      className="btn" 
-                                      style={{backgroundColor:"#21a00b", color:"#fff", outline:"none"}} 
-                                      onClick={this.getRecipe} type="submit">Search Recipe</Button>   
-                              </Form>                                   
-                          </div>
-                      </header>
+              <Form inline className="search" ref={(el) => this.myFormRef = el}>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Input type="text" placeholder="Search recipe..." onChange={this.onInputChange}  />
+                </FormGroup>{' '}
+                <Button 
+                  className="btn" 
+                  style={{backgroundColor:"#21a00b", color:"#fff", outline:"none"}} 
+                  onClick={this.getRecipe} type="submit">Search Recipe</Button>   
+              </Form>                                   
+            </div>
+          </header>
 
-                      <div className='sweet-loading'>
-                          <ClipLoader 
-                              size={80}
-                              color={'#21a00b'} 
-                              loading={this.state.loading} />
-                      </div>
+          <div className='sweet-loading'>
+              <ClipLoader 
+                size={80}
+                color={'#21a00b'} 
+                loading={this.state.loading} />
+          </div>
                       
-                      <RecipeList 
-                          keywords={this.state.keywords} 
-                          recipes={this.state.recipes}
-                          favorites={this.state.favorites} 
-                          addToFavorites={this.addToFavorites} /> 
-
-         
-                          
-                      <div>
-                      </div>
+          <RecipeList 
+            keywords={this.state.keywords} 
+            recipes={this.state.recipes}
+            favorites={this.state.favorites} 
+            addToFavorites={this.addToFavorites} /> 
                     
-                      {/* <FavoriteRecipes
-                        keywords={this.state.keywords} 
-                        recipes={this.state.recipes}
-                        favorites={this.state.favorites}
-                        addToFavorites={this.addToFavorites}
-                      /> */}
+            {/* <FavoriteRecipes
+              keywords={this.state.keywords} 
+              recipes={this.state.recipes}
+              favorites={this.state.favorites}
+              addToFavorites={this.addToFavorites}
+            /> */}
                   
-              </Container>
-          </Fragment>
-      )
+        </Container>
+      </Fragment>
+    )
   }
 
 }
 
 const actions = {
-    setRecipes
+  setRecipes
 }
 
 export default connect(null, actions)(App);
--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardDeck } from 'reactstrap';
 import ScrollAnimation from 'react-animate-on-scroll';
 import Modal from './Modal';
@@ -7,42 +7,73 @@ import { connect } from 'react-redux';
 import { favoriteRecipe } from '../actions';
 
 
-const RecipeItem = (props) => {
-  const {id, keyId, recipe, image_url, title, publisher_url, publisher, source_url, keywords, limitRecipeTitle} = props; 
+class RecipeItem extends Component {
+    state = {
+        favorited: false
+    }
+
+    favorite(recipe) {
+        this.props.favoriteRecipe(recipe);
+        this.setState({ favorited: true });
+    }
+    
+    render() {
+        const {
+            id, 
+            keyId, 
+            recipe, 
+            image_url, 
+            title, 
+            publisher_url, 
+            publisher, 
+            source_url, 
+            keywords, 
+            limitRecipeTitle} = this.props; 
+
+        return (
+          <Fragment>
+              
+              <Col md={4} sm={6} xs={4}>
+                  <ScrollAnimation animateIn="zoomIn" animateOnce>
+                      <CardDeck style={{border: "1px solid #ccc", padding:"20px 5px"}}>
+                          <Card body className="text-center">
+                              <CardImg top width="100%" src={image_url} alt="Card image cap" />
+                              <CardBody  style={{ padding: "10px"}}>
+                                  <CardTitle style={{ fontSize: "14px", marginTop:"16px"}}>{limitRecipeTitle(title)}</CardTitle>
+                                  <CardSubtitle 
+                                      style={{fontSize: "13px"}}>
+                                      Publisher: <a href={publisher_url} target="_blank">{publisher}</a>
+                                      {
+                                        this.state.favorited ? 
+                                            <div className="yellow-star" title="Already Added to Favorites">
+                                                <span> &#9733; </span>
+                                            </div>
+                                        : 
+                                            <div className="star" title="Add to Favorites" onClick={() => this.favorite(recipe)}>
+                                                <span> &#9733; </span>
+                                            </div>
+                                      }
+                                      
+                                  </CardSubtitle>
+                                  <Modal
+                                      key={keyId}
+                                      keywords={keywords}
+                                      id={id}
+                                      title={title}
+                                      publisher_url={publisher_url}
+                                      publisher_name={publisher}
+                                      image_url={image_url}
+                                      source_url={source_url}
+                                      />                   
+                              </CardBody>
+                          </Card>
+                      </CardDeck>
+                  </ScrollAnimation>
+              </Col>
+          </Fragment>
+        )
+    }
   
-  return (
-    <Fragment>
-        <Col md={4} sm={6} xs={4}>
-            <ScrollAnimation animateIn="zoomIn" animateOnce>
-                <CardDeck style={{border: "1px solid #ccc", padding:"20px 5px"}}>
-                    <Card body className="text-center">
-                        <CardImg top width="100%" src={image_url} alt="Card image cap" />
-                        <CardBody  style={{ padding: "10px"}}>
-                            <CardTitle style={{ fontSize: "14px", marginTop:"16px"}}>{limitRecipeTitle(title)}</CardTitle>
-                            <CardSubtitle 
-                                style={{fontSize: "13px"}}>
-                                Publisher: <a href={publisher_url} target="_blank">{publisher}</a>
-                                <div className="star" onClick={() => props.favoriteRecipe(recipe)}>
-                                    <span> &#9733; </span>
-                                </div>
-                            </CardSubtitle>
-                            <Modal
-                                key={keyId}
-                                keywords={keywords}
-                                id={id}
-                                title={title}
-                                publisher_url={publisher_url}
-                                publisher_name={publisher}
-                                image_url={image_url}
-                                source_url={source_url}
-                                />                   
-                        </CardBody>
-                    </Card>
-                </CardDeck>
-            </ScrollAnimation>
-        </Col>
-    </Fragment>
-  )
 }
 
 export default connect(null, { favoriteRecipe })(RecipeItem);
